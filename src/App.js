34e6e46092da444680f9913e6f7ec84c21624111
kareem badawy <52352856+kareem-r@users.noreply.github.com>
2023-05-04 03:21:32 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Notfound from './pages/Notfound';
+import Users from './pages/Users';
+import { ToastContainer} from 'react-toastify';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+   <>
+    <BrowserRouter>
+    <Routes>
+<Route path='/' element={<Home/> }/>
+<Route path='/login' element={<Login/> }/>
+<Route path='/signup' element={<Signup/> }/>
+<Route path='/users' element={<Users/> }/>
+<Route path='/*' element={<Notfound/> }/>
+<Route path='users/:id' element={<Profile></Profile>} />
+
+
+    </Routes>
+    </BrowserRouter>
+    <ToastContainer/>
+   </>
+  );
+}
+
+export default App;
